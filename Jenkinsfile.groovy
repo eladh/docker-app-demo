5cccdf5e13810b4858e5318867a15867ed699853
Elad Hirsch <eladh@jfrog.com>
2019-07-21 19:26:42 +0300

    dind

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -52,13 +52,9 @@ podTemplate(label: 'dind-template' , cloud: 'k8s' , containers: [
                     newImage = docker.build(dockerImageTag, "--build-arg DOCKER_REGISTRY_URL=docker.$rtIpAddress .")
                     newImageLatest = docker.build(dockerImageTagLatest, "--build-arg DOCKER_REGISTRY_URL=docker.$rtIpAddress .")
 
-//
-//                    newImage.push()
-//                    newImageLatest.push()
 
-
-                    buildInfo = rtDocker.push(dockerImageTag, "docker-local", buildInfo)
-
+                    newImage.push()
+                    newImageLatest.push()
 
                     server.publishBuildInfo buildInfo
 
